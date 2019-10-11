c74d1784dddeeef6fdbcf7a6414785ffbd50c3b6
adibfuadi <adibfuadimuhammad@gmail.com>
2019-10-11 15:22:05 +0700

    ok

--- lib/beranda/HomePage.dart
+++ lib/beranda/HomePage.dart
@@ -3,27 +3,31 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2
-        ),
-        itemBuilder: (BuildContext context, int index){
-          return Card(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 20,
-                ),
-                Image.asset("assets/booking.png", height: 80, width: 80,),
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text("Service Motor", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
-                )
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: MediaQuery.of(context).size.width / (MediaQuery.of(context).size.height/2.0)
+          ),
+          itemBuilder: (BuildContext context, int index){
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Image.asset("assets/booking.png", height: 80, width: 80,),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text("Service Motor", style: TextStyle(fontSize: 16, height: 1.2 ,fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
+                  )
 
-              ],
-            ),
-          );
-        }
+                ],
+              ),
+            );
+          }
+      ),
     );
   }
 }
